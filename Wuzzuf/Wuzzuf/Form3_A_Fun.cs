a9bb83334ed36a93a67418e8dfd13e674cda9cd5
Salah Mostafa <33065229+ZulaMostafa@users.noreply.github.com>
2020-04-13 23:26:21 +0200

    Done A.1, A.2

--- Wuzzuf/Wuzzuf/Form3_A_Fun.cs
+++ Wuzzuf/Wuzzuf/Form3_A_Fun.cs
@@ -14,7 +14,7 @@ namespace Wuzzuf
 {
     public partial class Form3_A_Fun : Form
     {
-        string ordb = "data source=orcl; user id=scott; password=tiger;";
+        string ordb = "data source=orcl; user id=scott; password=scott;";
         OracleConnection conn;
         string selectedColumn;
 
@@ -29,8 +29,7 @@ namespace Wuzzuf
 
         private void showCompaniesButton_Click(object sender, EventArgs e)
         {
-            conn = new OracleConnection(ordb);
-            conn.Open();
+           
 
             OracleCommand cmd = new OracleCommand();
             cmd.Connection = conn;
@@ -48,6 +47,44 @@ namespace Wuzzuf
         private void SelectColumnComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_A_Fun_Load(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(ordb);
+            conn.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select TYPENAME from JOBTYPES";
+            cmd.CommandType = CommandType.Text;
+
+            OracleDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                jobTypesComboBox.Items.Add(dr[0]);
+            }
+            dr.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = @"SELECT * FROM jobposts
+                                WHERE jobtypeid = (Select JOBTYPEID FROM jobtypes Where typename = :jobtype)";
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("jobtype", jobTypesComboBox.Items[jobTypesComboBox.SelectedIndex].ToString());
+            DataSet tables = new DataSet();
+            OracleDataAdapter Adapter = new OracleDataAdapter(cmd);
+            Adapter.Fill(tables);
+            DataGrid.DataSource = tables.Tables[0];
         }
     }
 }

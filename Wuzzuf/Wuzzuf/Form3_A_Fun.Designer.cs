bdf73a0cfb5814bc2a3149f8f662c13c31ee685d
salama135 <43586099+salama135@users.noreply.github.com>
2020-04-13 22:07:14 +0200

    Fun_A
    sada

---
+++ Wuzzuf/Wuzzuf/Form3_A_Fun.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Wuzzuf
+{
+    partial class Form3_A_Fun
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.showCompaniesButton = new System.Windows.Forms.Button();
+            this.selectColumnComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 323);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // showCompaniesButton
+            // 
+            this.showCompaniesButton.Location = new System.Drawing.Point(640, 70);
+            this.showCompaniesButton.Name = "showCompaniesButton";
+            this.showCompaniesButton.Size = new System.Drawing.Size(148, 28);
+            this.showCompaniesButton.TabIndex = 1;
+            this.showCompaniesButton.Text = "Show Companies";
+            this.showCompaniesButton.UseVisualStyleBackColor = true;
+            this.showCompaniesButton.Click += new System.EventHandler(this.showCompaniesButton_Click);
+            // 
+            // selectColumnComboBox
+            // 
+            this.selectColumnComboBox.FormattingEnabled = true;
+            this.selectColumnComboBox.Location = new System.Drawing.Point(488, 74);
+            this.selectColumnComboBox.Name = "selectColumnComboBox";
+            this.selectColumnComboBox.Size = new System.Drawing.Size(121, 24);
+            this.selectColumnComboBox.TabIndex = 2;
+            this.selectColumnComboBox.SelectedIndexChanged += new System.EventHandler(this.SelectColumnComboBox_SelectedIndexChanged);
+            // 
+            // Form3_A_Fun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.selectColumnComboBox);
+            this.Controls.Add(this.showCompaniesButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form3_A_Fun";
+            this.Text = "Form3_A_Fun";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button showCompaniesButton;
+        private System.Windows.Forms.ComboBox selectColumnComboBox;
+    }
+}
---
+++ Wuzzuf/Wuzzuf/Form3_A_Fun.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace Wuzzuf
+{
+    public partial class Form3_A_Fun : Form
+    {
+        string ordb = "data source=orcl; user id=scott; password=tiger;";
+        OracleConnection conn;
+        string selectedColumn;
+
+        public Form3_A_Fun()
+        {
+            InitializeComponent();
+
+
+
+
+        }
+
+        private void showCompaniesButton_Click(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(ordb);
+            conn.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select COMPANYNAME from companies";
+            cmd.CommandType = CommandType.Text;
+
+            OracleDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                selectColumnComboBox.Items.Add(dr[0]);
+            }
+            dr.Close();
+        }
+
+        private void SelectColumnComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
